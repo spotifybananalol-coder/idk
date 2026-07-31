884048b946673cbfdc077f76f67244dd2a771efa
spotifybananalol-coder <spotifybananalol@gmail.com>
2026-07-31 11:34:42 +0200

    amonguslel

--- new.lua
+++ new.lua
@@ -36,6 +36,21 @@ list.BackgroundTransparency=1;list.TextColor3=Color3.fromRGB(255,255,255)
 list.TextXAlignment=Enum.TextXAlignment.Left;list.TextYAlignment=Enum.TextYAlignment.Top
 list.Font=Enum.Font.Gotham;list.TextSize=13
 
+-- drag
+local UIS=game:GetService("UserInputService")
+local drag=false;local off
+list.InputBegan:Connect(function(i)
+	if i.UserInputType==Enum.UserInputType.MouseButton1 or i.UserInputType==Enum.UserInputType.Touch then
+		drag=true;off=i.Position-Box.AbsolutePosition
+	end
+end)
+UIS.InputEnded:Connect(function(i)if i.UserInputType==Enum.UserInputType.MouseButton1 or i.UserInputType==Enum.UserInputType.Touch then drag=false end end)
+UIS.InputChanged:Connect(function(i)
+	if drag and(i.UserInputType==Enum.UserInputType.MouseMovement or i.UserInputType==Enum.UserInputType.Touch)then
+		Box.Position=UDim2.fromOffset(i.Position.X-off.X,i.Position.Y-off.Y)
+	end
+end)
+
 while true do
 	local out=""
 	for _,p in ipairs(P:GetPlayers())do
